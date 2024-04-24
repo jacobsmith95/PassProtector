0bf33bd73e1a5ed5403a0f2563f6b0494582ed91
Jacob O. Smith <jacobsmith95@gmail.com>
2024-04-24 15:53:08 -0700

    updated database init

--- backend/database_init.py
+++ backend/database_init.py
@@ -1,8 +1,5 @@
 import motor.motor_asyncio
-import os
 
 def init_database():
-    client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
-    database = client.get_database()
-    collection = database.get_collection()
-    return collection
+    client = motor.motor_asyncio.AsyncIOMotorClient("MONGODB_URL")
+    return client

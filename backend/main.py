6d3761578c27da85fb33d070942b3aa2e417b4bf
Jacob O. Smith <jacobsmith95@gmail.com>
2024-04-30 21:27:20 -0700

    removed if main

--- backend/main.py
+++ backend/main.py
@@ -54,6 +54,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
+uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
     

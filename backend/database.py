7cebc596a42fc3d52be1bee4f86931c502b5d487
Jacob O. Smith <jacobsmith95@gmail.com>
2024-05-02 14:59:31 -0700

    refactored routes, introduced response schemas, and moved  database CRUD functions into one file

---
+++ backend/database.py
@@ -0,0 +1,118 @@
+import motor.motor_asyncio
+
+
+db_details = "MONGODB_URL"
+
+client = motor.motor_asyncio.AsyncIOMotorClient(db_details)
+database = client["user_database"]
+collection = database["Users"]
+
+
+#Helper Functions:
+
+def user_helper(user) -> dict:
+    """
+    takes a user returned from the database and gives a dict object
+    """
+    dict = {
+    "id"    : str(user["_id"]),
+    "email" : user["email"],
+    "hash"  : user["hash"],
+    "token" : user["token"],
+    "vault" : user["vault"]
+    }
+
+    return dict
+
+
+#Database CRUD Functions
+
+async def add_user(user_data: dict):
+    """
+    
+    """
+    result = await collection.insert_one(user_data)
+    new_user = await collection.find_one({"_id": result.inserted_id})
+    return user_helper(new_user)
+
+
+async def find_user(hash: str):
+    """
+    
+    """
+    user = await collection.find_one({"hash": hash})
+    if user is None:
+        return "failure"
+    else:
+        return user_helper(user)
+
+
+async def auth_user(hash: str):
+    """
+    
+    """
+    user = await collection.find_one({"hash": hash})
+    if user is None:
+        return "failure"
+    else:
+        return "success"
+    
+
+async def find_vault(hash: str):
+    """
+    
+    """
+    user = await collection.find_one({"hash": hash})
+    if user is None:
+        return "failure"
+    else:
+        user_dict = user_helper(user)
+        vault = user_dict["vault"]
+        return vault
+
+
+async def update_user(hash: str, new_data: dict):
+    """
+    
+    """
+    if len(new_data) < 1:
+        return "failure"
+    user = await collection.find_one({"hash": hash})
+    if user is not None:
+        updated = await collection.update_one({"_id": user["_id"]}, {"$set": new_data})
+        if updated is None:
+            return "failure to update"
+        updated_user = await collection.find_one({"_id": updated.upserted_id})
+        return user_helper(updated_user)
+    else:
+        return "failure"
+
+
+async def update_vault(hash: str, new_vault: str):
+    """
+    
+    """
+    user = await collection.find_one({"hash": hash})
+    if user is not None:
+        updated = await collection.update_one({"_id": user["_id"]}, {"$set": {"vault": new_vault}})
+        if updated is None:
+            return "failure to update vault"
+        updated_user = await collection.find_one({"_id": updated.upserted_id})
+        return user_helper(updated_user)
+    else:
+        return "failure"
+
+
+async def delete_user(hash: str):
+    """
+    
+    """
+    user = await collection.find_one({"hash": hash})
+    if user is not None:
+        result = await collection.delete_one({"_id": user["_id"]})
+        if result is None:
+            return "failure"
+        return "success"
+    else:
+        return "failure"
+
--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,8 @@
-from fastapi import Depends, FastAPI, HTTPException, status, Response, Body
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import FastAPI, HTTPException, status, Response, Body
 from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
-from typing import Annotated
-from backend.user_routes import router as UserRouter
-from backend.models import User
-import backend.database_init as dbi
+from models import UserSchema, UserUpdate, HashSchema, LoginResponseSchema, LoginErrorSchema
+from database import add_user, find_user, auth_user, find_vault, update_user, update_vault, delete_user
 import uvicorn
 import os
 
@@ -15,43 +12,28 @@ origins = [
     "https://frontend-ngnhr6tt3a-ul.a.run.app"
 ]
 
-app.include_router(UserRouter, tags=["Users"], prefix="/authenticated")
 
-client = dbi.init_database()
-
-async def get_collection():
-    db = await client["user_database"]
-    collection = await db["Users"]
-    return collection
-
-@app.post(path="/login/{hash}/", response_description="got user", status_code=status.HTTP_200_OK, response_model=User)
-async def login(hash: str, response: Response):
+@app.post(path="/login/", status_code=status.HTTP_200_OK)
+async def login(hash: HashSchema):
     """
     
     """
-    collection = await get_collection()
-    user = await collection.find_one({"hash": hash})
-    if user is None:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    result = await auth_user(hash)
+    if result == "success":
+        vault = await find_vault(hash)
+        return LoginResponseSchema(vault)
     
-    else:
-        response.status_code = status.HTTP_204_NO_CONTENT
-        return response
 
 
-@app.post(path="/create-account/", response_description="create a new user", status_code=status.HTTP_201_CREATED, response_model=User)
-async def create_user(user: User = Body(...)):
-    """
-    Creates a new user in the Users collection and returns the created user's entry as defined by the User model
-    """
-    collection = await get_collection()
-    new_user = jsonable_encoder(user)
-    result = await collection.insert_one(new_user)
-    created_user = await collection.find_one({"_id": result.inserted_id})
-    if created_user is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"user not created")
+#@app.post(path="/account-create/", response_description="create a new user", status_code=status.HTTP_201_CREATED, response_model=User)
+#async def create_user(user: User = Body(...)):
+#    """
+#    
+#    """
+    
 
-    return created_user
+
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -61,6 +43,4 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
     
